b4e57d9b5004d570060865b88d0a79301adb9875
refiansyahenno88-crypto <refiansyahenno88@gmail.com>
2026-06-20 07:25:05 -0700

    Add click event to display success message

---
+++ script.js
@@ -0,0 +1,9 @@
+const tombol = document.getElementById("btn");
+const pesan = document.getElementById("pesan");
+
+tombol.addEventListener("click", function(){
+
+    pesan.innerHTML =
+    "🎉 Selamat! Anda berhasil menjalankan JavaScript.";
+
+});
